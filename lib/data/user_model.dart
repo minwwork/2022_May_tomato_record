7cf41a8863df54d78fcae54f880085c2b70ab1db
minwwork <minwwork@gmail.com>
2022-06-05 04:36:03 +0900

    -user_model 2nd

--- lib/data/user_model.dart
+++ lib/data/user_model.dart
@@ -11,4 +11,41 @@ class UserModel{
   late DateTime createdDate;
   DocumentReference? reference;
 
+  UserModel({
+    required this.userkey,
+    required this.phoneNumber,
+    required this.address,
+    required this.lat,
+    required this.lan,
+    required this.geoFirePoint,
+    required this.createdDate,
+    required this.reference,});
+
+  UserModel.fromJson(Map<String, dynamic> json, this.userkey, this.reference) {
+    userkey = json['userkey'];
+    phoneNumber = json['phoneNumber'];
+    address = json['address'];
+    lat = json['lat'];
+    lan = json['lan'];
+    geoFirePoint = GeoFirePoint((json['geoFirePoint']['geopoint']).latitude, (json['geoFirePoint']['geopoint']).longitude);
+    createdDate = json['createdDate'] == null
+        ?DateTime.now().toUtc()
+        :(json['createdData'] as Timestamp).toDate();
+    reference = json['reference'];
+
+    Map<String, dynamic> toJson() {
+      final map = <String, dynamic>{};
+      map['phoneNumber'] = phoneNumber;
+      map['address'] = address;
+      map['lat'] = lat;
+      map['lan'] = lan;
+      map['geoFirePoint'] = geoFirePoint.data;
+      map['createdDate'] = createdDate;
+      return map;
+    }
+
+  }
+
+
+
 }
