918a12f75476a98aa96ee9f362873c3c025761ee
minwwork <minwwork@gmail.com>
2022-06-09 00:55:35 +0900

    -chat screen bottom bar(has error: item_service)

--- lib/screens/chat/chatroom_screen.dart
+++ lib/screens/chat/chatroom_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ChatroomScreen extends StatefulWidget {
-
   final String chatroomKey;
 
   const ChatroomScreen({Key? key, required this.chatroomKey}) : super(key: key);
@@ -13,7 +12,49 @@ class ChatroomScreen extends StatefulWidget {
 class _ChatroomScreenState extends State<ChatroomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellowAccent,
-    child: Text(widget.chatroomKey),);
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.yellowAccent,
+            )),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.grey,
+                    )),
+                Expanded(
+                    child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: '메세지를 입력하세요.',
+                      isDense: true,
+                      fillColor: Colors.white,
+                      filled: true,
+                      suffixIcon: GestureDetector(
+                        onTap: (){
+                          print('icon clicked');
+                        },
+                          child: Icon(Icons.emoji_emotions_outlined, color: Colors.grey,)),
+                      suffixIconConstraints: BoxConstraints.tight(Size(40,40)),
+                      contentPadding: EdgeInsets.all(10),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.grey))),
+                )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.send, color: Colors.grey))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
